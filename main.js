4850b04c346d2e9c17a0f8aba6ac78e3834f1f30
Nupur <singhnupur1404@gmail.com>
2024-05-31 14:58:48 +0530

    modulo operation fixed

--- main.js
+++ main.js
@@ -3,6 +3,11 @@ let buttons = document.querySelectorAll('.button') ;
 Array.from(buttons).forEach((button)=>{
     button.addEventListener('click',(e)=>{
         if(e.target.innerHTML == '='){
+            if(string.includes('%')){
+                string = string.replace('%','*') ;
+                string = `(${string}) / 100` ;
+                console.log(string) ;
+            }
             string = eval(string) ;
             document.querySelector('input').value = string ;
         }
@@ -10,10 +15,10 @@ Array.from(buttons).forEach((button)=>{
             string = " " ;
             document.querySelector('input').value = string ;
         }
-        else if(e.target.innerHTML == '%'){
-            string ="" ;
-            document.querySelector('input').value = string ;
-        }
+        // else if(e.target.innerHTML == '%'){
+        //     string ="" ;
+        //     document.querySelector('input').value = string ;
+        // }
         else{
             console.log(e.target)
             string = string + e.target.innerHTML ;
